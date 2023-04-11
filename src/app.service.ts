41319b4736643b91dfa6c9b5fb54c87319e397fb
Maki Jurowski <18458620+makijurowski@users.noreply.github.com>
2023-04-11 02:43:12 +0000

    Add customizations

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+
+  async callGPT4API(input: string): Promise<string> {
+    const apiKey = this.configService.get<string>('GPT4_API_KEY');
+    const apiUrl = 'https://gpt-4-api.openai.com/v1/engines/davinci-codex/completions';
+
+    try {
+      const response = await axios.post(
+        apiUrl,
+        {
+          prompt: input,
+          max_tokens: 50,
+          n: 1,
+          stop: null,
+          temperature: 0.5,
+          top_p: 1,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`,
+          },
+        },
+      );
+
+      return response.data.choices[0].text.trim();
+    } catch (error) {
+      console.error('Error while calling GPT-4 API:', error);
+      return 'Error while processing the request.';
+    }
   }
 }
